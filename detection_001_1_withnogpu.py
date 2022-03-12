31303a9b678088869c82779c7f184883e7537a84
JnxyJInxed <64402575+JnxyJInxed@users.noreply.github.com>
2022-03-13 04:28:01 +0700

    D:\Project\NgoprekSpace\darknetwithCPU003

--- detection_001_1_withnogpu.py
+++ detection_001_1_withnogpu.py
@@ -1,5 +1,4 @@
 # import dependencies
-from IPython.display import display, Javascript, Image
 from base64 import b64decode, b64encode
 import cv2
 import numpy as np
@@ -11,8 +10,17 @@ import matplotlib.pyplot as plt
 
 from darknet import *
 
+from pydantic import BaseModel
+from fastapi import FastAPI
+import pickle
+import json
+import base64
+from PIL import Image
+
+# DARKNET YOLO DETECTION PART
+
 # load in our YOLOv4 architecture network
-network, class_names, class_colors = load_network("cfg/yolov4-csp.cfg", "cfg/coco.data", "yolov4-csp.weights")
+network, class_names, class_colors = load_network("cfg/yolov4.cfg", "cfg/coco.data", "yolov4.weights")
 width = network_width(network)
 height = network_height(network)
 
@@ -35,16 +43,46 @@ def darknet_helper(img, width, height):
   return detections, width_ratio, height_ratio
 
 # run test on person.jpg image that comes with repository
-image = cv2.imread("data/person.jpg")
-detections, width_ratio, height_ratio = darknet_helper(image, width, height)
+def test_Darknet():
+  image = cv2.imread("D:/Project/NgoprekSpace/darknetwithCPU003/darknet/data/person.jpg")
+  detections, width_ratio, height_ratio = darknet_helper(image, width, height)
 
-for label, confidence, bbox in detections:
-  left, top, right, bottom = bbox2points(bbox)
-  left, top, right, bottom = int(left * width_ratio), int(top * height_ratio), int(right * width_ratio), int(bottom * height_ratio)
-  cv2.rectangle(image, (left, top), (right, bottom), class_colors[label], 2)
-  cv2.putText(image, "{} [{:.2f}]".format(label, float(confidence)),
-                    (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    class_colors[label], 2)
-cv2_imshow(image)
+  for label, confidence, bbox in detections:
+    left, top, right, bottom = bbox2points(bbox)
+    left, top, right, bottom = int(left * width_ratio), int(top * height_ratio), int(right * width_ratio), int(bottom * height_ratio)
+    cv2.rectangle(image, (left, top), (right, bottom), class_colors[label], 2)
+    cv2.putText(image, "{} [{:.2f}]".format(label, float(confidence)),
+                      (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                      class_colors[label], 2)
+  # cv2.imshow('', image)
+  # cv2.waitKey(0)
 
-detections
+  print(detections)
+
+  if (detections != []):
+    return (str(detections) + '\nDarknet Sucessfully installed')
+  else:
+    return (str(detections) + '\nThere is a problem with Darknet installation')
+
+
+# FAST API PART
+app = FastAPI()
+
+@app.on_event("startup")
+#load model etc
+
+@app.get('/')
+def index():
+    from datetime import datetime
+    
+    time = datetime.now()
+    message = 'This is the homepage of the API at ' + str(time)
+
+    testResult = test_Darknet()
+    return {'message': message, 'Test Result': testResult}
+
+# RUN SERVER
+from colabcode import ColabCode
+server = ColabCode(port=800, code=False)
+
+server.run_app(app=app)
